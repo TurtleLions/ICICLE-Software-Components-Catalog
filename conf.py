8b22cb075f0aea59b117d9a547564a50df515ade
TurtleLions <ryanestanislao@gmail.com>
2023-09-15 20:42:47 -0700

    attempt

--- conf.py
+++ conf.py
@@ -48,3 +48,8 @@ templates_path = ['templates']
 
 html_theme = 'sphinx_rtd_theme'
 html_logo = './_static/icicle.jpeg'
+html_context = {
+"display_github": False, # Add 'Edit on Github' link instead of 'View page source'
+"last_updated": True,
+"commit": False,
+}
